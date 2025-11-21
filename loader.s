3b9abf132b464db6f784ba9585e3ff91b76df187
TheFrostBunny <David.aa.leren@gmail.com>
2025-11-21 10:48:18 +0100

    lagt til mere i loader og gjordt sånna at kernal bruker magicnummer

--- loader.s
+++ loader.s
@@ -1,2 +1,31 @@
+.set MAGIC, 0x1BADB002
+.set FLAGS, (1<<0 | 1<<1)
+.set CHECKSUM, -(MAGIC + FLAGS)
+
+.section .multiboot
+  .long MAGIC
+  .long FLAGS
+  .long CHECKSUM
+
+
+
 .section .text
 .extrn kernalMain
+.global loader
+
+loader:
+    mov &kernal_stack ,%esp
+    call kernalMain
+    push %eax
+    push %ebx
+
+_stop:
+    cli
+    hlt
+    jmp _stop
+
+
+
+.section .bss
+.space 2*1024*1024 # 2 MB for kernel stack
+kernal_stack:
